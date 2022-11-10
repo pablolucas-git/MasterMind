783c9db2a0dd8c7499a664c63a103cb6c3f5c2ff
javi <javi@javi.es>
2022-11-10 11:05:34 +0100

    htufo

--- src/java/mastermindpackage/DatabaseConnection.java
+++ src/java/mastermindpackage/DatabaseConnection.java
@@ -6,9 +6,15 @@ package mastermindpackage;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -16,17 +22,14 @@ import java.util.logging.Logger;
  */
 public class DatabaseConnection {
 
-    protected Connection c;
+    protected Connection con;
 
     public DatabaseConnection() throws SQLException {
         String user = "user";
         String password = "pass";
         String driver = "com.mysql.cj.jdbc.Driver";
-        String nombre = "nombre";
-        String puntos = "puntos";
-        String intentos = "intentos";
         String dbName = "mastermind";
-        c = null;
+        con = null;
         String Url = "jbdc:mysql://192.168.33.23:3306/" + dbName + "?"
                 + "enabledTLSPprotocols=TLSv1.3"
                 + "&autoReconnect=true"
@@ -34,10 +37,56 @@ public class DatabaseConnection {
         
         try {
             Class.forName(driver);
-            c = DriverManager.getConnection(Url, user, password);
-        } catch (ClassNotFoundException ex) {
+            con = DriverManager.getConnection(Url, user, password);
+        } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
+    
+    public boolean openedDatabase(){
+        return con != null;
+    }
+    
+    public boolean guardarJugador(String nombre, int intentos, int puntos){
+        
+        boolean jugadorGuardado = false;
+        
+        try {
+            PreparedStatement st = this.con
+                    .prepareStatement("insert into mastermind values(?, ?, ?)");
+            st.setString(1, nombre);
+            st.setInt(2, puntos);
+            st.setInt(3, intentos);
+            st.executeUpdate();
+            st.close();
+            jugadorGuardado = true;
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return jugadorGuardado;
+    }
+    
+    public List<String[]> obtenerJugador(){
+        
+        List<String[]> datosJugador = new ArrayList<>();
+        
+        String consulta = "SELECT * FROM mastermind";        
+        try {
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(consulta);
+            
+            while (rs.next()){
+                String[] datos = new String[3];
+                datos[0] = rs.getString("nombre");
+                datos[1] = rs.getString("puntos");
+                datos[2] = rs.getString("intentos");
+                datosJugador.add(datos);
+            }
+            return datosJugador;
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+    
 }
